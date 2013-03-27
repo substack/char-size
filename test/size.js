ec3117117ef8cf4e12db2d955646562058f7acab
James Halliday <mail@substack.net>
2013-03-26 20:01:19 -0700

    test

---
+++ test/size.js
@@ -0,0 +1,12 @@
+var test = require('tape');
+var charSize = require('../');
+
+test('character sizes', function () {
+    t.plan(5);
+    var size = charSize(document.body);
+    t.equal(typeof size.width, 'number');
+    t.equal(typeof size.height, 'number');
+    t.ok(size.width > 4);
+    t.ok(size.height > 10);
+    t.ok(size.width < size.height);
+});
